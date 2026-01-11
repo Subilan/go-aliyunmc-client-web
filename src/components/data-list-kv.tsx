c250a69ec5ec5c760895b934c3907ec6d1f258c0
Subilan <suyuanliang1995@gmail.com>
2026-01-11 16:49:15 +0800

    change primary color, add permission display in profile

--- src/components/data-list-kv.tsx
+++ src/components/data-list-kv.tsx
@@ -9,34 +9,36 @@ export default function DataListKv(props: { data: Record<string, boolean | numbe
 		<div className={cn('gap-4', props.grid ? 'grid grid-cols-2' : 'flex flex-col', props.border ? 'border border-neutral-200 p-3 rounded-lg' : '', props.className)}>
 			{Object.entries(props.data).map(([k, v]) => (
 				<div className="flex items-center gap-2" key={k}>
-					<div className="w-25 text-neutral-500">{k}</div>
-					{typeof v === 'boolean' && (v ? <CheckIcon size={14} /> : <XIcon size={14} />)}
-					{(typeof v === 'string' || typeof v === 'number') && <div>{v}</div>}
-					{typeof v === 'object' &&
-						(() => {
-							const child = (
-								<div className="flex gap-2 items-center">
-									{v.content}
-									{v.copy && (
-										<Button size={'icon-xs'} variant={'outline'}>
-											<CopyIcon />
-										</Button>
-									)}
-									{v.render}
-								</div>
-							);
+					<div className='w-25 text-neutral-500'>{k}</div>
+					<div>
+						{typeof v === 'boolean' && (v ? <CheckIcon size={14} /> : <XIcon size={14} />)}
+						{(typeof v === 'string' || typeof v === 'number') && v}
+						{typeof v === 'object' &&
+							(() => {
+								const child = (
+									<div className="flex gap-2 items-center">
+										{v.content}
+										{v.copy && (
+											<Button size={'icon-xs'} variant={'outline'}>
+												<CopyIcon />
+											</Button>
+										)}
+										{v.render}
+									</div>
+								);
 
-							return v.detail ? (
-								<Tooltip>
-									<TooltipTrigger asChild>{child}</TooltipTrigger>
-									<TooltipContent>
-										<p>{v.detail}</p>
-									</TooltipContent>
-								</Tooltip>
-							) : (
-								child
-							);
-						})()}
+								return v.detail ? (
+									<Tooltip>
+										<TooltipTrigger asChild>{child}</TooltipTrigger>
+										<TooltipContent>
+											<p>{v.detail}</p>
+										</TooltipContent>
+									</Tooltip>
+								) : (
+									child
+								);
+							})()}
+					</div>
 				</div>
 			))}
 		</div>
--- src/components/dialogs/index/ProfileDialog.tsx
+++ src/components/dialogs/index/ProfileDialog.tsx
@@ -1,15 +1,17 @@
+import DataListKv from '@/components/data-list-kv';
 import type { DialogControl } from '@/components/dialogs/type';
 import Wrapper from '@/components/dialogs/Wrapper';
 import { Button } from '@/components/ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Spinner } from '@/components/ui/spinner';
 import { LS_KEY_USER_LOGIN_TOKEN } from '@/consts';
-import { UserPayloadContext } from '@/contexts/UserPayloadContext';
+import { UserPayloadContext, type UserPayload } from '@/contexts/UserPayloadContext';
 import { req } from '@/lib/req';
 import times from '@/lib/times';
 import { router } from '@/root';
 import type { User } from '@/types/User';
-import { useContext, useEffect, useState } from 'react';
+import { InfoIcon } from 'lucide-react';
+import { useContext, useEffect, useMemo, useState } from 'react';
 
 async function getSelf() {
 	const { data, error } = await req<User>('/user', 'get');
@@ -21,10 +23,23 @@ function logout() {
 	router.navigate({ to: '/lor' });
 }
 
+const adminPermissions = ['创建实例', '部署实例', '删除实例', '关闭服务器', '触发备份或归档'];
+const defaultPermissions = ['一键创建', '开启服务器'];
+
+function getRolePermissionDisplay(role: UserPayload['role']): Record<string, boolean> {
+	let entries: [string, boolean][] = [];
+	if (role === 'user') entries = adminPermissions.map(x => [x, false]).concat(defaultPermissions.map(x => [x, true])) as any;
+	if (role === 'admin') entries = adminPermissions.concat(defaultPermissions).map(x => [x, true]);
+
+	return Object.fromEntries(entries);
+}
+
 export default function ProfileDialog(props: DialogControl) {
 	const [user, setUser] = useState<User>();
 	const userPayload = useContext(UserPayloadContext);
 	const [loading, setLoading] = useState(false);
+
+	const permissionDisplay = useMemo(() => getRolePermissionDisplay(userPayload.role), [userPayload.role]);
 
 	useEffect(() => {
 		setLoading(true);
@@ -54,6 +69,17 @@ export default function ProfileDialog(props: DialogControl) {
 							<div className="font-bold">权限组</div>
 							<p>
 								{user.role === 'user' && '普通用户'} {user.role === 'admin' && '超级用户'}
+								<Popover>
+									<PopoverTrigger asChild>
+										<Button variant={'ghost'} size={'icon-xs'}>
+											<InfoIcon size={12} />
+										</Button>
+									</PopoverTrigger>
+									<PopoverContent className="w-[200px]">
+										<p>{user.role === 'user' ? '普通用户' : '超级用户'}可以做什么？</p>
+										<DataListKv data={permissionDisplay} />
+									</PopoverContent>
+								</Popover>
 							</p>
 						</div>
 						<div className="flex flex-col gap-2">
