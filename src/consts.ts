0cce79a20b7afba0f227b9e0d3829ed7c61647bf
Subilan <suyuanliang1995@gmail.com>
2026-01-21 13:22:15 +0800

    change api base

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://8.217.241.208:33761'
+export const API_BASE = 'https://st-backend.subilan.win'
 // export const API_BASE = 'http://127.0.0.1:33791'
 export const KEY_NAMESPACE = 'gomc-client-web__';
 export const LS_KEY_USER_LOGIN_TOKEN = `${KEY_NAMESPACE}_ls_user_login_token`;
