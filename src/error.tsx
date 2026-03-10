40e9b13a51bacc10a360350c72004d9c32868a7a
Subilan <suyuanliang1995@gmail.com>
2026-03-10 15:50:52 +0800

    update error boundary page

--- src/error.tsx
+++ src/error.tsx
@@ -1,14 +1,14 @@
-import CenterCardPage from '@/components/center-card-page';
+import { AlertCircleIcon } from 'lucide-react';
 
 export default function ErrorBoundary(props: any) {
 	return (
-		<CenterCardPage title='无法加载'>
-			<p>这可能是因为后端服务崩溃或者其它更为严重的原因，详细信息可参考控制台的输出。</p>
-			<p className="text-neutral-500">请联系系统维护者以解决问题。</p>
-			<details>
-				<summary>报错信息</summary>
-				{JSON.stringify(props)}
-			</details>
-		</CenterCardPage>
+		<div className="h-dvh w-dvw flex items-center justify-center">
+			<div className="flex flex-col gap-2 items-center">
+				<div className="text-xl flex items-center gap-2">
+					<AlertCircleIcon /> 无法进入控制台
+				</div>
+				<p>后端服务暂时不可用</p>
+			</div>
+		</div>
 	);
 }
